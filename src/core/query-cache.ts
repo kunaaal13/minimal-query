cff302b27703aaaa2a886690b9d3c070b7323fb3
kunaaal13 <kunaaal.rao@gmail.com>
2025-05-18 13:46:20 +0530

    Enhance QueryCache and Query classes: add caching methods, state management, and fetch functionality for improved data handling.

--- src/core/query-cache.ts
+++ src/core/query-cache.ts
@@ -1,10 +1,51 @@
-import Query from './query'
+import { hashKey } from './hash'
+import Query, { QueryConfig } from './query'
+import { QueryKey } from './types'
 
 class QueryCache {
   private queries: Map<string, Query>
 
   constructor() {
     this.queries = new Map<string, Query>()
+  }
+
+  clear() {
+    this.queries.clear()
+  }
+
+  getQueryFromCache<T = unknown, E = Error>(
+    key: QueryKey
+  ): Query<T, E> | undefined {
+    const hashedKey = hashKey(key)
+    return this.queries.get(hashedKey) as Query<T, E>
+  }
+
+  buildQuery<T = unknown, E = Error>(
+    queryConfig: QueryConfig<T, E>
+  ): Query<T, E> {
+    // Check if the query is already in the cache, if so, return it
+    const existingQuery = this.getQueryFromCache<T, E>(queryConfig.key)
+    if (existingQuery) {
+      return existingQuery
+    }
+
+    // If not, create a new query and add it to the cache
+    const query = new Query<T, E>(queryConfig)
+    this.addQuery(query)
+
+    return query
+  }
+
+  addQuery<T = unknown, E = Error>(query: Query<T, E>) {
+    if (!this.queries.has(query.queryHash)) {
+      this.queries.set(query.queryHash, query as Query)
+    }
+  }
+
+  removeQuery(query: Query) {
+    if (this.queries.has(query.queryHash)) {
+      this.queries.delete(query.queryHash)
+    }
   }
 }
 
--- src/core/query.ts
+++ src/core/query.ts
@@ -1,20 +1,84 @@
+import { hashKey } from './hash'
 import { QueryKey, QueryOptions, QueryState } from './types'
 
-type QueryConfig<T = unknown, E = Error> = {
+export type QueryConfig<T = unknown, E = Error> = {
   key: QueryKey
   options: QueryOptions
   state: QueryState<T, E>
+  fetchFn: () => Promise<T>
 }
 
 class Query<T = unknown, E = Error> {
   private key: QueryKey
+  queryHash: string
   private options: QueryOptions
   private state: QueryState<T, E>
+  private fetchFn: () => Promise<T>
+  private activePromise: Promise<T> | undefined = undefined
+  private subscribers: Set<() => void> = new Set()
 
-  constructor({ key, options, state }: QueryConfig<T, E>) {
+  constructor({ key, options, state, fetchFn }: QueryConfig<T, E>) {
     this.key = key
+    this.queryHash = hashKey(key)
     this.options = options
     this.state = state
+    this.fetchFn = fetchFn
+  }
+
+  // Update the state of the query - this is used to update the state of the query
+  // updateFn is a function that takes the current state and returns the new state
+  updateState(updateFn: (state: QueryState<T, E>) => QueryState<T, E>) {
+    this.state = updateFn(this.state)
+  }
+
+  // This is the main function that fetches the data from the API
+  // Storing a promise in the class instance allows us to avoid race conditions when multiple calls to fetch are made
+  // If a promise is already active, return it
+  async fetch() {
+    if (this.activePromise) {
+      return this.activePromise
+    }
+
+    const executeFetch = async (): Promise<T> => {
+      // 1. Update state to loading and fetching
+      this.updateState((state) => ({
+        ...state,
+        fetchStatus: 'fetching',
+        error: undefined,
+      }))
+
+      // 2. Fetch data
+      try {
+        const data = await this.fetchFn()
+        this.updateState((state) => ({
+          ...state,
+          data,
+          status: 'success',
+        }))
+
+        return data
+      } catch (error) {
+        this.updateState((state) => ({
+          ...state,
+          data: undefined,
+          status: 'error',
+          error: error as E,
+        }))
+
+        throw error
+      } finally {
+        // 3. Update state to idle
+        this.updateState((state) => ({
+          ...state,
+          fetchStatus: 'idle',
+        }))
+
+        // 4. Reset the promise to undefined to allow for a new fetch
+        this.activePromise = undefined
+      }
+    }
+
+    return await executeFetch()
   }
 }
 
